702a3e5e33ee16cd5c7047e4ca8b5552aef9eb47
github167 <catcat161-github@yahoo.com.hk>
2021-10-05 11:37:16 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,20 +1,4 @@
-# https://github.com/Zokrates/ZoKrates
-curl -LSfs get.zokrat.es | sh
-
-cat << EOF >root.zok
-def main(private field a, field b) -> bool:
-  return a * a == b
-EOF
-
-# compile
-zokrates compile -i root.zok
-# perform the setup phase
-zokrates setup
-# execute the program
-zokrates compute-witness -a 337 113569
-# generate a proof of computation
-zokrates generate-proof
-# export a solidity verifier
-zokrates export-verifier
-# or verify natively
-zokrates verify
+git clone https://github.com/kendricktan/hello-world-zk-dapp.git
+cd hello-world-zk-dapp
+npm install
+npm run start
